2f4bbee76524742059fa3d8f089634b03c9a15eb
WillReardon <wer29@drexel.edu>
2016-11-14 15:41:25 -0500

    Code / Environment Update
    Updated the wall and enemy codes to be what I had last week. Also began
    working on environment tweaks

--- Assets/ActualProject/Scripts/EnemyController.cs
+++ Assets/ActualProject/Scripts/EnemyController.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class EnemyController : MonoBehaviour
 {
     public float speed;
+    public int damage;
     public Vector3 targetPosition;
-    
-
+    //public Transform muzzlepoint;
 
 	// Use this for initialization
 	void Start ()
@@ -16,7 +16,7 @@ public class EnemyController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime); 
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition/*muzzlepoint.position*/, speed * Time.deltaTime); 
 	}
 
 
@@ -24,6 +24,21 @@ public class EnemyController : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Bullet"))
         {
+            Destroy(gameObject);
+        }
+
+        else if (other.gameObject.CompareTag("Wall"))
+        {
+            // Get a reference to a health script attached to the collider we hit
+            WallControl health = other.gameObject.GetComponent<WallControl>();
+
+            // If there was a health script attached
+            if (health != null)
+            {
+                // Call the damage function of that script, passing in our enemy damage variable
+                health.Damage(damage);
+            }
+
             Destroy(gameObject);
         }
     }
---
+++ Assets/ActualProject/Scripts/WallControl.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallControl : MonoBehaviour
+{
+    //The wall's current health point total
+    public int currentHealth = 3;
+    Material mat = new Material(Shader.Find("Standard"));
+
+    void Start()
+    {
+        MeshRenderer mr = gameObject.GetComponent<MeshRenderer>();
+        
+        mat.color = Color.white;
+        //assign the material to renderer
+        mr.material = mat;
+    }
+
+
+    public void Damage(int damageAmount)
+    {
+        //subtract damage amount when Damage function is called
+        currentHealth -= damageAmount;
+
+        //Check if health has fallen below zero
+        if (currentHealth <= 0)
+        {
+            //if health has fallen below zero, deactivate it 
+            gameObject.SetActive(false);
+        }
+        
+        if (currentHealth >= 0)
+        {
+            TextureChange(currentHealth);
+        }
+    }
+
+
+
+    void TextureChange(int currentHealth)
+    {
+        if (currentHealth == 3)
+        {
+            mat.color = Color.white;
+        }
+        if (currentHealth == 2)
+        {
+            mat.color = Color.blue;
+        }
+        if (currentHealth == 1)
+        {
+            mat.color = Color.red;
+        }
+    }
+}
