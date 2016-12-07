c74569ebd35bf7a997f12f5a6fa985bdd429a87a
andthology <andthology@gmail.com>
2016-12-06 20:40:47 -0500

    main menu exit game button script

---
+++ Assets/ActualProject/Scripts/main_menu_scripts/button_exitgame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class button_exitgame : MonoBehaviour {
+
+	public Button exitButton;
+
+    void Start()
+    {
+        Button btn = exitButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+    }
+
+}
